28c016ab139043b8d2afdcceab55e8149f6dff72
Shivam Srivastava <shivam.362000.1@gmail.com>
2022-04-01 18:18:32 +0530

    Cursor movement added

--- src/com/sriv/shivam/Main.java
+++ src/com/sriv/shivam/Main.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
@@ -20,6 +21,7 @@ public class Main {
 
     public static void main(String[] args) throws AWTException {
         Robot robot = new Robot();
+        String[] array = new String[2];
 
         try {
             serverSocket = new ServerSocket(47430);
@@ -30,14 +32,11 @@ public class Main {
         System.out.println("Server listening to port 47430");
 
         while(!message.equalsIgnoreCase("over")) {
-            System.out.println("While loop");
-
             try {
-                System.out.println("Try block");
                 // the accept method waits for a new client connection
                 // and returns an individual socket for that connection
                 clientSocket = serverSocket.accept();
-                System.out.println("Client Socket = " + clientSocket.getLocalPort());
+//                System.out.println("Client Socket = " + clientSocket.getLocalPort());
 
                 // get the inputstream from socket, which will have
                 // the message from the clients
@@ -46,13 +45,15 @@ public class Main {
 
                 // reading the message
                 message = bufferedReader.readLine();
+                array = message.split("&");
 
                 // printing the message
-                System.out.println(message);
+                System.out.println(Arrays.toString(array));
 
-                if(Objects.equals(message, "L")) {
-                    robot.mouseMove(100, 500);
-                }
+                int x = Integer.parseInt(array[0]);
+                int y = Integer.parseInt(array[1]);
+
+                robot.mouseMove(x, y);
 
                 // finally it is very important
                 // that you close the sockets
